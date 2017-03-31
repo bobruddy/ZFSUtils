1a392e86123b5d98e77f8b03abaf0f4802403bab
Robert Ruddy <bob@ruddy.net>
2017-03-31 17:33:29 -0500

    Initial version of snapping root and greating a recovery clone

--- scripts/rootSnap.sh
+++ scripts/rootSnap.sh
@@ -2,4 +2,26 @@
 
 # This script will snapshot off root and also create the recovery clone
 
+# Setup
 TIMESTAMP=$(date +%G%m%e%H%M)
+rootFS="rpool/ROOT/ubuntu"
+rootSnap="${rootFS}@${TIMESTAMP}"
+recoveryClone="${rootFS}-recovery"
+recoveryMount="/ubuntu-recovery"
+
+echo "Creating snap: ${rootSnap}"
+zfs snapshot ${rootSnap}
+
+echo "Destroying old root clone: ${recoveryClone}"
+umount ${recoveryMount}
+zfs destroy ${recoveryClone}
+
+echo "Create new root clone: ${rootSnap} -> ${recoveryClone}"
+zfs clone ${rootSnap} ${recoveryClone}
+
+echo "Set mountpoint on new root clone: ${recoveryClone}"
+zfs set mountpoint=legacy ${recoveryClone}
+mount ${recoveryMount}
+
+echo "Update Grub"
+update-grub
